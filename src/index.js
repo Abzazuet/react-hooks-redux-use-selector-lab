6591650f9cff92e350c7d3661d6a33d2084ded01
Abel Zazueta <abzazuet@amazon.com>
2022-09-15 17:41:54 -0700

    Assignment completeD

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import App from "./App";
-
-import usersReducer from "./features/users/usersSlice";
-
 // add imports and code
+import usersReducer from "./features/users/usersSlice";
+import { Provider } from "react-redux"; /* code change */
+import { createStore } from "redux"; /* code change */
+
+const store = createStore(usersReducer);
 
 ReactDOM.render(
   // add imports and code
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   // add imports and code
   document.getElementById("root")
 );
